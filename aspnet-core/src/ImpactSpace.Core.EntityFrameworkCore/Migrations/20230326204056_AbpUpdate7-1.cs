bbf1b3b314995e01eed864909f242e3c2bce46c0
Berin Iwlew <beriniwlew@protonmail.com>
2023-03-26 22:42:54 +0200

    Updated Blazorise, EF migration for ABP 7.1

---
+++ aspnet-core/src/ImpactSpace.Core.EntityFrameworkCore/Migrations/20230326204056_AbpUpdate7-1.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ImpactSpace.Core.Migrations
+{
+    /// <inheritdoc />
+    public partial class AbpUpdate71 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "EntityVersion",
+                table: "AbpUsers",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "EntityVersion",
+                table: "AbpTenants",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "EntityVersion",
+                table: "AbpRoles",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "EntityVersion",
+                table: "AbpOrganizationUnits",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "EntityVersion",
+                table: "AbpUsers");
+
+            migrationBuilder.DropColumn(
+                name: "EntityVersion",
+                table: "AbpTenants");
+
+            migrationBuilder.DropColumn(
+                name: "EntityVersion",
+                table: "AbpRoles");
+
+            migrationBuilder.DropColumn(
+                name: "EntityVersion",
+                table: "AbpOrganizationUnits");
+        }
+    }
+}
