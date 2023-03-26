21ae20d11b2618f85424f8e350eafcb94c8744d4
Berin Iwlew <beriniwlew@protonmail.com>
2023-03-26 07:23:44 +0200

    Added a logger

--- aspnet-core/src/ImpactSpace.Core.Blazor/CoreBlazorModule.cs
+++ aspnet-core/src/ImpactSpace.Core.Blazor/CoreBlazorModule.cs
@@ -13,6 +13,7 @@ using ImpactSpace.Core.Blazor.Menus;
 using ImpactSpace.Core.EntityFrameworkCore;
 using ImpactSpace.Core.Localization;
 using ImpactSpace.Core.MultiTenancy;
+using Microsoft.Extensions.Logging;
 using OpenIddict.Validation.AspNetCore;
 using Volo.Abp;
 using Volo.Abp.Account.Web;
@@ -61,6 +62,12 @@ namespace ImpactSpace.Core.Blazor;
    )]
 public class CoreBlazorModule : AbpModule
 {
+    private readonly ILogger<CoreBlazorModule> _logger;
+
+    public CoreBlazorModule(ILogger<CoreBlazorModule> logger)
+    {
+        _logger = logger;
+    }
     public override void PreConfigureServices(ServiceConfigurationContext context)
     {
         context.Services.PreConfigure<AbpMvcDataAnnotationsLocalizationOptions>(options =>
@@ -92,7 +99,7 @@ public class CoreBlazorModule : AbpModule
     {
         var configuration = context.Services.GetConfiguration();
         var hostingEnvironment = context.Services.GetHostingEnvironment();
-        
+        _logger.LogInformation("Environment: " + hostingEnvironment.EnvironmentName);
         PreConfigure<AbpOpenIddictAspNetCoreOptions>(options =>
         {
             if (hostingEnvironment.IsProduction())
