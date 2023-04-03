36063b093f0645fb346bbb21af86b4f48a77f26c
Berin Iwlew <beriniwlew@protonmail.com>
2023-04-03 07:03:35 +0200

    Challenge concurrency stamp

---
+++ aspnet-core/src/ImpactSpace.Core.EntityFrameworkCore/Migrations/20230403050311_ChallengeConcurrencyStamp.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ImpactSpace.Core.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChallengeConcurrencyStamp : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ConcurrencyStamp",
+                table: "AppChallenges",
+                type: "character varying(40)",
+                maxLength: 40,
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ConcurrencyStamp",
+                table: "AppChallenges");
+        }
+    }
+}
