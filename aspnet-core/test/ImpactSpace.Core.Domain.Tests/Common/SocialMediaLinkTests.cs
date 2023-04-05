6fc645d59b3faef5920437ad72ea9152265aabbf
Berin Iwlew <beriniwlew@protonmail.com>
2023-04-05 10:43:31 +0200

    Social Media Link tests

---
+++ aspnet-core/test/ImpactSpace.Core.Domain.Tests/Common/SocialMediaLinkTests.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Collections.Generic;
+using ImpactSpace.Core.Common;
+using ImpactSpace.Core.Organizations;
+using Shouldly;
+using Volo.Abp.Validation;
+using Xunit;
+
+namespace ImpactSpace.Core.Common
+{
+    public class SocialMediaLinkTests : CoreDomainTestBase
+    {
+        [Fact]
+        public void Should_Create_SocialMediaLink()
+        {
+            var socialMediaLink = new SocialMediaLink(SocialMediaPlatform.Twitter, "https://twitter.com/Impact_Space");
+
+            socialMediaLink.Platform.ShouldBe(SocialMediaPlatform.Twitter);
+            socialMediaLink.Url.ShouldBe("https://twitter.com/Impact_Space");
+        }
+
+        [Fact]
+        public void Should_Throw_Validation_Exception_For_Invalid_Url()
+        {
+            Assert.Throws<ArgumentException>(() =>
+            {
+                var socialMediaLink = new SocialMediaLink(SocialMediaPlatform.Facebook, "invalid url");
+            });
+        }
+
+        [Theory]
+        [MemberData(nameof(ValidUrls))]
+        public void Should_Set_Valid_Url(string validUrl)
+        {
+            var socialMediaLink = new SocialMediaLink(SocialMediaPlatform.LinkedIn, validUrl);
+
+            socialMediaLink.Platform.ShouldBe(SocialMediaPlatform.LinkedIn);
+            socialMediaLink.Url.ShouldBe(validUrl);
+        }
+
+        public static IEnumerable<object[]> ValidUrls => new List<object[]>
+        {
+            new object[] { "https://www.linkedin.com/company/impact-space/" },
+            new object[] { "https://www.facebook.com/ImpactSpaceHQ" },
+            new object[] { "https://www.instagram.com/impact_space/" },
+            new object[] { "https://www.youtube.com/channel/UCsPGkNvTJZ0h3qawbIjCr-w" },
+        };
+    }
+}
