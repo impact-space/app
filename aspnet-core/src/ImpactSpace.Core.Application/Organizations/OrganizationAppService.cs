2e01bd185d6132619dbe744d504fb5946ec6d24a
Berin Iwlew <beriniwlew@protonmail.com>
2023-04-07 04:59:01 +0200

    Checking for null arguments now in Org App Service

--- aspnet-core/src/ImpactSpace.Core.Application/Organizations/OrganizationAppService.cs
+++ aspnet-core/src/ImpactSpace.Core.Application/Organizations/OrganizationAppService.cs
@@ -1,8 +1,10 @@
 using System;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using System.Threading.Tasks;
 using ImpactSpace.Core.Permissions;
 using Microsoft.AspNetCore.Authorization;
+using Volo.Abp;
 using Volo.Abp.Application.Dtos;
 using Volo.Abp.Domain.Repositories;
 
@@ -28,8 +30,10 @@ public class OrganizationAppService : CoreAppService, IOrganizationAppService
         return ObjectMapper.Map<Organization, OrganizationDto>(organization);
     }
 
-    public async Task<PagedResultDto<OrganizationDto>> GetListAsync(GetOrganizationListDto input)
+    public async Task<PagedResultDto<OrganizationDto>> GetListAsync([NotNull] GetOrganizationListDto input)
     {
+        Check.NotNull(input, nameof(input));
+        
         if(input.Sorting.IsNullOrWhiteSpace())
         {
             input.Sorting = nameof(Organization.Name);
@@ -52,8 +56,10 @@ public class OrganizationAppService : CoreAppService, IOrganizationAppService
         );
     }
     
-    public async Task<OrganizationDto> CreateAsync(CreateOrganizationDto input)
+    public async Task<OrganizationDto> CreateAsync([NotNull] CreateOrganizationDto input)
     {
+        Check.NotNull(input, nameof(input));
+        
         var tenantId = CurrentTenant.Id!.Value;
         
         var organization = await _organizationManager.CreateAsync(
@@ -65,8 +71,9 @@ public class OrganizationAppService : CoreAppService, IOrganizationAppService
         return ObjectMapper.Map<Organization, OrganizationDto>(organization);
     }
     
-    public async Task UpdateAsync(Guid id, UpdateOrganizationDto input)
+    public async Task UpdateAsync(Guid id, [NotNull] UpdateOrganizationDto input)
     {
+        Check.NotNull(input, nameof(input));
         await _organizationManager.UpdateAsync(id, input.Name, input.Description);
     }
 
