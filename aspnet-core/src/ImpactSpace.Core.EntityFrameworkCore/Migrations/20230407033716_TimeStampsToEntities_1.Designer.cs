76a9b7df546fcddd8e8d8f722d45c6ef2f86777e
Berin Iwlew <beriniwlew@protonmail.com>
2023-04-07 05:38:04 +0200

    EF Migrations for new concurrencystamps

--- aspnet-core/src/ImpactSpace.Core.EntityFrameworkCore/Migrations/20230407033716_TimeStampsToEntities_1.Designer.cs
+++ aspnet-core/src/ImpactSpace.Core.EntityFrameworkCore/Migrations/20230407033716_TimeStampsToEntities_1.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using ImpactSpace.Core.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using Volo.Abp.EntityFrameworkCore;
@@ -12,9 +13,11 @@ using Volo.Abp.EntityFrameworkCore;
 namespace ImpactSpace.Core.Migrations
 {
     [DbContext(typeof(CoreDbContext))]
-    partial class CoreDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230407033716_TimeStampsToEntities_1")]
+    partial class TimeStampsToEntities1
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ aspnet-core/src/ImpactSpace.Core.EntityFrameworkCore/Migrations/20230407033716_TimeStampsToEntities_1.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ImpactSpace.Core.Migrations
+{
+    /// <inheritdoc />
+    public partial class TimeStampsToEntities1 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ConcurrencyStamp",
+                table: "AppTags",
+                type: "character varying(40)",
+                maxLength: 40,
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ConcurrencyStamp",
+                table: "AppProjectTags",
+                type: "character varying(40)",
+                maxLength: 40,
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ConcurrencyStamp",
+                table: "AppProjectSkills",
+                type: "character varying(40)",
+                maxLength: 40,
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ConcurrencyStamp",
+                table: "AppProjectChallenges",
+                type: "character varying(40)",
+                maxLength: 40,
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ConcurrencyStamp",
+                table: "AppProjectCategories",
+                type: "character varying(40)",
+                maxLength: 40,
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ConcurrencyStamp",
+                table: "AppTags");
+
+            migrationBuilder.DropColumn(
+                name: "ConcurrencyStamp",
+                table: "AppProjectTags");
+
+            migrationBuilder.DropColumn(
+                name: "ConcurrencyStamp",
+                table: "AppProjectSkills");
+
+            migrationBuilder.DropColumn(
+                name: "ConcurrencyStamp",
+                table: "AppProjectChallenges");
+
+            migrationBuilder.DropColumn(
+                name: "ConcurrencyStamp",
+                table: "AppProjectCategories");
+        }
+    }
+}
